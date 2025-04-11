85279aa84fb8733e48387dca1438992fb2b644c4
vitormiguel99 <158460149+vitormiguel99@users.noreply.github.com>
2025-04-11 15:06:43 +0200

    Update app.py

--- TheDashboard/app.py
+++ TheDashboard/app.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import pandas as pd
-from ml import model  # your model.py
-from utils.helpers import preprocess_data  # optional
+from ml import model 
+from utils.helpers import preprocess_data  
 
-# Title
+
 st.title("Machine Learning Dashboard")
 
-# Upload CSVs or read from folder
+
 data_file = st.file_uploader("Upload your CSV", type=["csv"])
 if data_file is not None:
     df = pd.read_csv(data_file)
     st.write("📊 Data Preview", df.head())
 
-    # Optional preprocessing
+    
     df_clean = preprocess_data(df)
 
-    # ML Predictions
+    
     if st.button("Run Prediction"):
         prediction = model.predict(df_clean)
         st.write("🧠 Predictions", prediction)
